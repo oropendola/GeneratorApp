0809f729310e4e61c5fcd5e93fc00144d5d76292
Javi <highavoc@yahoo.es>
2020-05-09 19:02:01 +0200

    Test routes. Add friendly_id gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  #get 'pages/home'
+  #get 'pages/about'
+  #get 'pages/contact'
+
+  resources :portfolios, except: [:show]
+
+
+  get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+
+
   resources :skills
   resources :guides
   resources :categories
   resources :blogs
-  resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  root to: "pages#home"
+
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+
 end
