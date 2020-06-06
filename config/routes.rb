bb2e7914dee6fac8fd89e26aa1d88ae2174b8323
Javi <highavoc@yahoo.es>
2020-06-06 20:02:25 +0200

    Add dynamic login links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'registrer' }
+
   resources :topics
   #get 'pages/home'
   #get 'pages/about'
