d43a2fde96684906fd6811a98b981ddedbf139d3
Javi <highavoc@yahoo.es>
2020-06-17 23:40:02 +0200

    Add draggable behaviour with javascript

---
+++ app/javascript/channels/portfolios.js
@@ -0,0 +1,25 @@
+// Codigo en coffe
+// ready = undefined
+//
+// ready = ->
+//  $('.sortable').sortable()
+//  return
+//
+// // el ready de jquery indica que cuando la pagina ha cargado le pasamos la funcion
+// // de la variable ready
+// $(document).ready ready
+
+var ready = undefined
+
+ready = function() {
+	
+  $('.sortable').sortable()
+  return
+
+}
+
+$(document).ready ready
+
+
+
+
