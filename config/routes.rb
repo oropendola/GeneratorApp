07d607bed2dcd28f254c89d23c0e562c5af26020
Javi <highavoc@yahoo.es>
2020-06-26 22:04:24 +0200

    Add topic page and links

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'registrer' }
 
-  resources :topics
+  resources :topics, only: [:index, :show]
+  
   #get 'pages/home'
   #get 'pages/about'
   #get 'pages/contact'
